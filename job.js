43a940670de9c940a47fe4e3e727979118146333
Victor Usen <usenvictor40@gmail.com>
2026-06-12 15:07:55 +0100

    Add logs to Post Queues, Set up uptime ping to keep server alive.

--- job.js
+++ job.js
@@ -1,16 +1,16 @@
-import http from "http";
+console.log("JOB.JS LOADED");
+ import http from "http";
 import cron from "node-cron";
 import { postToFacebook } from "./controller/facebookController.js";
 import { scheduleFixturesForToday } from "./services/scheduleFixtures.js";
 
 // Location to queue all jobs/posts to be posted to facebook
-
-export let jobs = [];
+export const jobs = [];
 
 // Initialize today's schedules on boot
-scheduleFixturesForToday(jobs);
+await scheduleFixturesForToday(jobs);
 
-// Schedule daily sync at 00:05 AM
+// Schedule daily sync at 08:00 AM
 cron.schedule("0 8 * * *", async () => {
     console.log("Daily sync: scheduling fixtures for the new day...");
     await scheduleFixturesForToday(jobs);
@@ -19,21 +19,18 @@ cron.schedule("0 8 * * *", async () => {
 cron.schedule("* * * * *", async () => {
     if (jobs.length === 0) return;
 
-    console.time("jobq");
     const jobsToProcess = jobs.splice(0);
 
     let i = 1;
     for (const job of jobsToProcess) {
-        console.time("job" + i);
-        console.log(job);
-        console.timeEnd("job" + i);
+        console.log("[QUEUED]: " + job );
 
-        console.time("post");
-        await postToFacebook(job);  
-        console.timeEnd("post");
+        console.time("POSTED: " + i + " " + job);
+        await postToFacebook(job); 
+        console.log("[POSTED]: ");
+        console.timeEnd("POSTED: " + i + " " + job);
         i++;
     };
-    console.timeEnd("jobq");
 });
 
 // Health-check server for Render (free web service tier requires HTTP)
@@ -46,3 +43,13 @@ http.createServer((req, res) => {
         uptime: process.uptime().toFixed(0) + "s"
     }));
 }).listen(PORT, () => console.log(`Health check server listening on port ${PORT}`));
+
+setInterval(async () => {
+    try {
+        console.log("Pinging up the Server...");
+        const res = await fetch('https://matchsub.onrender.com');
+        console.log("Server is up again\n\n", res.status, "\n\n");
+    } catch (error) {
+        console.log("Server is down\n\n", error.message, "\n\n");
+    }
+}, 780000);
