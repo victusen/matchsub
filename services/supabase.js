1a1fbd04520a49b374922f15f5e5e97ed60aeb55
Victor Usen <usenvictor40@gmail.com>
2026-06-12 15:18:15 +0100

    install supabase sdk library, and create boiler insert fucntion to table

---
+++ services/supabase.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
+const Url = process.env.SUPABASE_URL;
+const Key = process.env.SUPABASE_ANON_KEY;
+
+export const supabase = createClient(Url, Key);
+
+export async function sendToSupabase ({fixture_id, home_team, away_team, kickoff_time, lineup_time }) {
+    try {
+        const { data, error } = await supabase
+            .from("fixtures")
+            .insert([
+                {
+                    "fixture_id": fixture_id,
+                    "home_team": home_team,
+                    "away_team": away_team,
+                    "kickoff_time": kickoff_time,
+                    "lineup_time": lineup_time                  
+                }
+            ]);
+
+        if (error) {
+            console.error("Supabase Error:", error);
+            return;
+        }
+    } catch (err) {
+        console.error("Unexpected Error:", err);
+    }
+    
+}; 
