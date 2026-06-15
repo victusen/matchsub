b442f391471c536237a363283a0d8a4de70c5509
Victor Usen <usenvictor40@gmail.com>
2026-06-15 16:15:53 +0100

    new config file to control player name issues. Previous - divide name into 2 arrayed, then pick last name, not effient, players like Vitinha, Ederson, Alisson Becker, etc, moving to a list of predefined names to be checked and reform to a preferred posting name. In progress

---
+++ config/names.js
@@ -0,0 +1,15 @@
+const names = {
+    "Ederson": "Ederson",
+    "Neymar Jnr": "Neymar",
+    "Vinicius Jnr": "Vinicius",
+    "Danilo": "Danilo",
+    "Alisson": "Alisson",
+    "Eder Militão": "Militao",
+    "Marquinhos": "Marquinhos",
+    "Antonio Rüdiger": "Rudiger",
+    
+
+    
+};
+
+export default names;
