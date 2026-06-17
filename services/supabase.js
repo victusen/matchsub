9da6a9c85495e0367f26e37066fd2457078f06cd
Victor Usen <usenvictor40@gmail.com>
2026-06-17 14:57:10 +0100

    comment supabase logic. Before Github mobile editor edit

--- services/supabase.js
+++ services/supabase.js
@@ -7,27 +7,63 @@ const Key = process.env.SUPABASE_ANON_KEY;
 
 const supabase = createClient(Url, Key);
 
-export default async function insertToSupabase({fixture_id, home_team, away_team, kickoff_time, lineup_time }) {
+// export async function insertToSupabase({fixture_id, home_team, away_team, kickoff_time, lineup_time }) {
+//     try {
+//         const { data, error } = await supabase
+//             .from("fixtures")
+//             .insert([
+//                 {
+//                     "fixture_id": fixture_id,
+//                     "home_team": home_team,
+//                     "away_team": away_team,
+//                     "kickoff_time": kickoff_time,
+//                     "lineup_time": lineup_time,
+//                     "date": date   
+//                 }
+//             ]);
+
+//         console.log("Forwarded Fixture:", home_team, "vs", away_team, "id: " + fixture_id);
+
+//         if (error) {
+//             console.error("Supabase Error:", error);
+//             return;
+//         }
+//     } catch (err) {
+//         console.error("Unexpected Error:", err);
+//     }
+// }; 
+
+export async function getFixtures(date) {
     try {
         const { data, error } = await supabase
             .from("fixtures")
-            .insert([
-                {
-                    "fixture_id": fixture_id,
-                    "home_team": home_team,
-                    "away_team": away_team,
-                    "kickoff_time": kickoff_time,
-                    "lineup_time": lineup_time                  
-                }
-            ]);
+            .select("*").eq("date", date);
 
-        console.log("Fixture saved:", home_team, "vs", away_team, "id: " + fixture_id);
-
-        if (error) {
-            console.error("Supabase Error:", error);
-            return;
+        if (data.length === 0) {
+            console.log("No Scheduled Matches Found");
+            return [];
         }
-    } catch (err) {
-        console.error("Unexpected Error:", err);
+
+        console.log(data);
+        console.log("LOGGING DATA");
+        return data;
+
+    } catch (error) {
+        console.log(error);
     }
-}; 
+};
+// console.log(await getFixtures("06/15/202"));
+
+async function updateFixtureStatus(e, d) {
+    const { data, error} = await supabase
+        .from("fixtures")
+        .update({
+            status: e === "live" ? "live" : "scheduled"
+        })
+        .eq("date", d)
+
+    console.log(data);
+    console.log(error);
+}
+
+// updateFixtureStatus("live", "06/16/2026");
